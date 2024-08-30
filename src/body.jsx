49e415c51f6b9db526687e5fcdd85eb33739ea06
Duncan Ramuhashi <duncanramohashi@gmail.com>
2024-08-30 14:49:42 +0200

    body and header done

--- src/body.jsx
+++ src/body.jsx
@@ -4,13 +4,13 @@ function Body() {
     return (
         <div className="hero bg-[#351a0e] text-white h-[700px] flex justify-between items-center px-20">
             <div className='w-[600px]'>
-                <h1 className='font-serif font-bold text-8xl pl-16'>
+                <h1 className='font-serif font-bold  text-5xl md:text-8xl md:pl-16'>
                     Grab the best ice cream.
                 </h1>
-                <p className="pl-16 pt-6 text-lg">
+                <p className="md:pl-16 pt-6 text-lg">
                 Our commitment to quality and innovation ensures that each scoop is a delightful experience, making Frosty Delights the perfect destination for anyone seeking a sweet escape.
                 </p>
-                <div className='pl-16 pt-6'>
+                <div className='md:pl-16 pt-6'>
                     <a href='#flav'>
                     <button className='bg-yellow-500 rounded-full w-[200px] h-[50px] text-xl text-black font-serif font-bold'>
                         Shop Flavors
@@ -20,7 +20,7 @@ function Body() {
                 </div>
             </div>
             <div className=''>
-                <img src={ice} alt="Ice Cream" className='h-[550px] w-[650px] rounded-2xl object-cover' />
+                <img src={ice} alt="Ice Cream" className='h-36 w-60  md:h-[550px] md:w-[650px] rounded-2xl object-cover' />
             </div>
         </div>
     );
--- src/hero.jsx
+++ src/hero.jsx
@@ -2,22 +2,30 @@
 
 function Hero(){
     return(
-        <div className="bg-[#351a0e] text-white">
-            <div className="flex pl-28 pt-8">
-            <header className="flex items-center pt-6 p-11">
-            <div className="logo text-4xl font-serif font-bold mr-96">Creamy</div>
-                <nav className="flex items-center">
-                    <ul className="flex list-none font-serif text-2xl">
-                        <li className="mr-10"><a href="#about">About</a></li>
-                        <li className="mr-10"><a href="#flav">Flavors</a></li>
-                        <li><a href="#contact">Contact Us</a></li>
-                    </ul>
-                </nav>
-            </header>
+        <div className="bg-[#351a0e] text-white w-fulls">
+    <div className="flex flex-col md:flex-row pl-4 md:pl-28 pt-8">
+        <header className="flex flex-col md:flex-row items-center pt-6 px-4 md:px-11">
+            <div className="text-2xl md:text-4xl font-serif font-bold mb-4 md:mb-0 md:mr-96">
+                Creamy
             </div>
-            <hr className="w-[1000px] h-[1px] ml-[150px] border-orange-200"></hr>
-            
-        </div>
+            <nav className="w-full">
+                <ul className="flex flex-col md:flex-row list-none font-serif text-xl md:text-2xl">
+                    <li className="mb-2 md:mb-0 md:mr-10">
+                        <a href="#about">About</a>
+                    </li>
+                    <li className="mb-2 md:mb-0 md:mr-10">
+                        <a href="#flav">Flavors</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact Us</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+    <hr className="border-t border-orange-200 mt-4 md:mt-8" />
+</div>
+
         
     );
 }
